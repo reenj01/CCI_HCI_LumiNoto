60499e62ddf4ea40b082ae69e875ccf13e46d4b1
IreenJ <ireenjeang0726@gmail.com>
2025-11-09 17:24:23 +0000

    test that single led works

---
+++ singleLED.py
@@ -0,0 +1,21 @@
+from gpiozero import LED
+from time import sleep
+
+# Initialize an LED connected to GPIO pin 17 using the GPIO Zero library.
+led = LED(17)
+
+
+while True:
+    # Turn on the LED and print a message to the console.
+    led.on()
+    print('LED ON')
+
+    # Wait for 0.5 seconds with the LED on.
+    sleep(0.5)
+
+    # Turn off the LED and print a message to the console.
+    led.off()
+    print('LED OFF')
+
+    # Wait for 0.5 seconds with the LED off.
+    sleep(0.5)
